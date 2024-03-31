ad7f7330120c605708af1c06ebfaf1093a72a939
XxxMaxLiar <xxxmaxliar@gmail.com>
2024-03-31 23:55:10 +0300

    Дополнил базу данных автомобилей для гаража Андрея Немяко

--- cars/models.py
+++ cars/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Car(models.Model):
-    number = models.IntegerField(help_text="Номер машины")
-    make = models.CharField(help_text="Фирма машины", max_length = 50)
-    color = models.CharField(max_length = 50)
+    number = models.IntegerField(help_text="Номер Машины")
+    make = models.CharField(help_text="Фирма Машины", max_length = 50)
+    color = models.CharField(max_length = 51)
     price = models.IntegerField()
 
     def __str__(self):
