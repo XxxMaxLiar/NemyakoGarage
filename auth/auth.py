d265aeb7c7f44a9f3fff5fa036a54966cbbd597e
XxxMaxLiar <xxxmaxliar@gmail.com>
2024-05-10 15:40:15 +0300

    refreshauthcod

--- auth/auth.py
+++ auth/auth.py
@@ -1,17 +1,17 @@
 import re
-from typing import Union
 
 def is_valid_password(password: str) -> bool:
-    # Проверяем, что пароль содержит буквы, цифры и спецсимволы
-    if not any(re.match(f'^{k}', password) for k in '0OIll1lIi@#$%^&*()_+-=[]{};:"/,?'):
+    """Проверяем, что пароль содержит буквы, цифры и спецсимволы"""
+    if not any(re.match(f'^{symbol}', password) for symbol in '0OIll1lIi@#$%^&*()_+-=[]{};:"/,?'):
         return False
 
-    # Проверяем, что пароль содержит буквы верхнего и нижнего регистра
+    """Проверяем, что пароль содержит буквы верхнего и нижнего регистра"""
     if not any(c.isupper() for c in password) or not any(c.islower() for c in password):
         return False
 
-    # Проверяем, что длина пароля больше минимальной (8 символов)
-    if len(password) < 8:
+    """Проверяем, что длина пароля больше минимальной (8 символов)"""
+    min_len_password = 8
+    if len(password) < min_len_password:
         return False
 
     return True
